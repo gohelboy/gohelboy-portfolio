02a92e12b739090918b579ef41c707795610469e
Dwarkesh Gohel <dwarkeshgohel1@gmail.com>
2024-04-05 00:42:40 +0530

    overflow fixed

--- app/_components/worked.jsx
+++ app/_components/worked.jsx
@@ -6,11 +6,11 @@ import { motion } from 'framer-motion'
 const Worked = () => {
 
     const icons = [
-        { name: 'Part Note', icon: <IconPN className={"text-[--secondary-bg] h-[100px] md:w-[150px] 2xl:w-[200px] opacity-80 hover:opacity-100 hover:-translate-y-1 transition-all"} /> },
-        { name: 'Inside', icon: <IconInside className={"text-[--secondary-bg] h-[100px] md:w-[150px] 2xl:w-[200px] opacity-80 hover:opacity-100 hover:-translate-y-1 transition-all"} /> },
-        { name: 'Property Watch', icon: <IconPW className={"text-[--secondary-bg] h-[100px] md:w-[150px] 2xl:w-[140px] opacity-80 hover:opacity-100 hover:-translate-y-1 transition-all "} /> },
-        { name: 'Bingepedia', icon: <IconBeingepedia className={"text-[--secondary-bg] h-[100px] md:w-[150px] 2xl:w-[200px] opacity-80 hover:opacity-100 hover:-translate-y-1 transition-all"} /> },
-        { name: 'Tiffin Service Propvider', icon: <IconTSP className={"text-[--secondary-bg] h-[100px] md:w-[150px] 2xl:w-[200px] opacity-80 hover:opacity-100 hover:-translate-y-1 transition-all"} /> },
+        { name: 'Part Note', icon: <IconPN className={"text-[--secondary-bg]  size-32 h-[100px] md:w-[150px] 2xl:w-[200px] opacity-80 hover:opacity-100 hover:-translate-y-1 transition-all"} /> },
+        { name: 'Inside', icon: <IconInside className={"text-[--secondary-bg] size-32 h-[100px] md:w-[150px] 2xl:w-[200px] opacity-80 hover:opacity-100 hover:-translate-y-1 transition-all"} /> },
+        { name: 'Property Watch', icon: <IconPW className={"text-[--secondary-bg] size-32 h-[100px] md:w-[150px] 2xl:w-[140px] opacity-80 hover:opacity-100 hover:-translate-y-1 transition-all "} /> },
+        { name: 'Bingepedia', icon: <IconBeingepedia className={"text-[--secondary-bg] size-32 h-[100px] md:w-[150px] 2xl:w-[200px] opacity-80 hover:opacity-100 hover:-translate-y-1 transition-all"} /> },
+        { name: 'Tiffin Service Propvider', icon: <IconTSP className={"text-[--secondary-bg] size-32 h-[100px] md:w-[150px] 2xl:w-[200px] opacity-80 hover:opacity-100 hover:-translate-y-1 transition-all"} /> },
     ]
 
     return (
@@ -19,7 +19,7 @@ const Worked = () => {
                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 1 }}
                 viewport={{ once: true, amount: 1 }}
-                className='flex flex-wrap items-center justify-between gap-1 md:gap-4 p-7 px-10 border border-[--border-color] w-full md:max-w-[1140px] 2xl:max-w-[1280px] rounded-3xl bg-gradient-to-b from-[#0a0a0a] via-[--primary-bg] to-[--primary-bg] '>
+                className='flex flex-wrap items-center justify-center md:justify-between gap-1 md:gap-4 p-7 px-10 border border-[--border-color] w-full md:max-w-[1140px] 2xl:max-w-[1280px] rounded-3xl bg-gradient-to-b from-[#0a0a0a] via-[--primary-bg] to-[--primary-bg] '>
                 {icons?.map((icon, i) => <motion.div
                     initial={{ y: 40, opacity: 0 }}
                     whileInView={{ y: 0, opacity: 1 }}
